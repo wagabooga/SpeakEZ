4bd28702fe7e7eaafe67c104dd2c097fe78e5cbe
wagabooga <mattanthu@hotmail.com>
2021-12-15 20:58:25 -0800

    added helper function and made messages an object. using message object created time and username functionality into the chat

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http')
 
 const express = require('express');
 const socketio = require('socket.io')
+const formatMessage = require('./helpers/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -11,25 +12,27 @@ const io = socketio(server)
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'SpeakEZ Bot'
+formatMessage(botName, 'Welcome to SpeakEZ!' )
 
 // Run when client connects
 io.on('connection', socket => {
   console.log('New socket connection...')
 
   // welcome message (single client)
-  socket.emit('message', 'Welcome to SpeakEZ!')
+  socket.emit('message', formatMessage(botName, 'Welcome to SpeakEZ!' ))
 
   // broadcast when a user connects (all clients !currentUser)
-  socket.broadcast.emit('message', 'A User has joined the chat');
+  socket.broadcast.emit('message', formatMessage(botName, 'A User has joined the chat' ));
 
   // runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat')
+    io.emit('message', formatMessage(botName, 'A user has left the chat' ))
   })
 
   // listen for incoming chat messages
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg)
+    io.emit('message', formatMessage('USER', msg))
   })
 })
 

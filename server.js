c1b223d0e069a7ce28de200d30683ee65dee4fc9
wagabooga <mattanthu@hotmail.com>
2021-12-15 20:28:28 -0800

    established socket connection with user + SpeakEZ

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const path = require('path')
+const http = require('http')
+
 const express = require('express');
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
 
+// set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Run when client connects
+io.on('connection', socket => {
+  console.log('New socket connection...')
+
+  socket.emit('message', 'Welcome to SpeakEZ!')
+})
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {console.log(`Server up on port ${PORT}`)})
+server.listen(PORT, () => {console.log(`Server up on port ${PORT}`)})
